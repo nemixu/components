3ac963e7a17aba0e7ce1fcca445198782837b358
nemixu <s.r.seagrave@gmail.com>
2023-01-05 16:45:20 +0000

    Playing around with buttons and classnames lib, and tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Children } from "react";
+import Button from "./components/Button";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <div>
+        <Button success>Click me</Button>
+      </div>
+      <div>
+        <Button primary>Buy Now</Button>
+      </div>
+      <div>
+        <Button warning>Close</Button>
+      </div>
+      <div>
+        <Button danger>Something!</Button>
+      </div>
+      <div>
+        <Button secondary outline rounded>
+          Next
+        </Button>
+      </div>
+      <div>
+        <Button rounded success>
+          Previous
+        </Button>
+      </div>
+      <div>
+        <Button outline>Test</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
